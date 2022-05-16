dfa17d4d798006a9c1a02bdb5f7be225336e6464
Alexandre Chastan <alex.c@mkey.fr>
2022-05-16 08:33:10 +0200

    Fix jsonSerialize return type

--- src/BaseObject.php
+++ src/BaseObject.php
@@ -55,7 +55,7 @@ class BaseObject implements JsonSerializable
     /**
      * @inheritDoc
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return [
             'uid' => $this->uid,
